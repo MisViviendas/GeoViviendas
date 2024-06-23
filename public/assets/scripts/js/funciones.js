26b397ee71bbc2d9c1a0371255e75f804ba1d895
Renato Reyes Valenzuela <u20221b471@upc.edu.pe>
2024-06-23 10:48:20 -0500

    Entrega TF

--- public/assets/scripts/js/funciones.js
+++ public/assets/scripts/js/funciones.js
@@ -5,3 +5,19 @@
     //window.open("formRegistro.html","_self");
 //}
 
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    var width = sidebar.style.width;
+    sidebar.style.width = width === "250px" ? "0" : "250px"; // Cambia el ancho para mostrar/ocultar
+}
+
+// Opcional: Cerrar la barra lateral al hacer clic fuera de ella
+document.addEventListener('click', function(event) {
+    var sidebar = document.getElementById("sidebar");
+    var isClickInsideSidebar = sidebar.contains(event.target);
+    var isSidebarOpen = sidebar.style.width === "250px";
+
+    if (!isClickInsideSidebar && isSidebarOpen) {
+        toggleSidebar(); // Cierra la barra lateral si está abierta y el clic fue fuera
+    }
+}, true);
